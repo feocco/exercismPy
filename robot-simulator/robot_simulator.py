b5233c1fd027b48fc62267960da20ec1e61a34b4
Joe Feocco <joefeocco@gmail.com>
2016-03-14 15:55:39 -0400

    Added  simulate, advance, and turn functions

--- robot-simulator/robot_simulator.py
+++ robot-simulator/robot_simulator.py
@@ -1,40 +1,52 @@
+class NORTH:
+	pass
+
+
+class SOUTH:
+	pass
+
+
+class EAST:
+	pass
+
+
+class WEST:
+	pass
+
+
 class Robot:
 	def __init__(self, direction=NORTH, xCoord=0, yCoord=0):
 		self.bearing = direction
-		self.coordinates = [xCoord, yCoord]
-
+		self.coordinates = (xCoord, yCoord)
+		self.directions = [NORTH, EAST, SOUTH, WEST]
 
 	def simulate(self, string):
-		# Move according to string
-
+		for command in string:
+			if command == 'L':
+				self.turn_left()
+			elif command == 'R':
+				self.turn_right()
+			else:
+				self.advance()
 
 	def advance(self):
-		# Move forward one unit
-
+		# Move one unit towards direction
+		x, y = self.coordinates
+		if self.bearing == NORTH:
+			y += 1
+		elif self.bearing == SOUTH:
+			y -= 1
+		elif self.bearing == EAST:
+			x += 1
+		else:
+			x -= 1
+		self.coordinates = (x, y)
 
 	def turn_right(self):
 		# Turn right
+		index = self.directions.index(self.bearing)
 
 
 	def turn_left(self):
 		# Turn left
-
-
-class NORTH:
-	def __init__(self):
-		self = self
-
-
-class EAST:
-	def __init__(self):
-		self = self
-
-
-class SOUTH:
-	def __init__(self):
-		self = self
-
-
-class WEST:
-	def __init__(self):
-		self = self
+		pass
