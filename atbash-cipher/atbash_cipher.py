e330449d46bdb0e3c0de18d69d50e525ee58bf90
Joe Feocco <joefeocco@gmail.com>
2016-03-15 15:07:17 -0400

    Added decode() and have passed all 10 tests

--- atbash-cipher/atbash_cipher.py
+++ atbash-cipher/atbash_cipher.py
@@ -1,25 +1,34 @@
 import re 
 
 def encode(string):
-	alph = 'abcdefghijklmnopqrstuvwxyz'
+	alph = 'abcdefghijklmnopqrstuvwxyz' # [::-1] Extended slice syntax for reversing string
 	resultString = ''
 	counter = 0
 
 	for letter in string:
-		#print('letter: ', letter)
-		if re.compile(r'[a-z]').match(letter.lower()): # If alphanumeric
-			#print('alpha match')
-			index = alph.index(letter.lower())
-			resultString += alph[-(index + 1)]
+		if re.compile(r'[a-z]').match(letter.lower()):
+			index = alph[::-1].index(letter.lower())
+			resultString += alph[index]
 			counter += 1
-		elif re.compile(r'\d').match(letter): # If digit
+		elif re.compile(r'\d').match(letter):
 			resultString += letter
 			counter += 1
 		if counter == 5:
 			counter = 0
 			resultString += ' '
-
+	if resultString[-1] == ' ':
+		return resultString[:-1]
 	return resultString
 
 def decode(string):
 	alph = 'abcdefghijklmnopqrstuvwxyz'
+	resultString = ''
+
+	for letter in string:
+		if re.compile(r'[a-z]').match(letter.lower()):
+			index = alph[::-1].index(letter.lower())
+			resultString += alph[index]
+		elif re.compile(r'\d').match(letter):
+			resultString += letter
+
+	return resultString
