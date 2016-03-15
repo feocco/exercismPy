56b95a18fe20ad095cbe2f92c16af839aaf032a2
Joe Feocco <joefeocco@gmail.com>
2016-03-15 15:20:51 -0400

    Encode() using string indexing and join for adding spaces

--- atbash-cipher/atbash_cipher.py
+++ atbash-cipher/atbash_cipher.py
@@ -3,22 +3,15 @@ import re
 def encode(string):
 	alph = 'abcdefghijklmnopqrstuvwxyz' # [::-1] Extended slice syntax for reversing string
 	resultString = ''
-	counter = 0
 
 	for letter in string:
 		if re.compile(r'[a-z]').match(letter.lower()):
 			index = alph[::-1].index(letter.lower())
 			resultString += alph[index]
-			counter += 1
 		elif re.compile(r'\d').match(letter):
 			resultString += letter
-			counter += 1
-		if counter == 5:
-			counter = 0
-			resultString += ' '
-	if resultString[-1] == ' ':
-		return resultString[:-1]
-	return resultString
+
+	return ' '.join([resultString[i:i+5] for i in range(0, len(resultString), 5)]) 
 
 def decode(string):
 	alph = 'abcdefghijklmnopqrstuvwxyz'
